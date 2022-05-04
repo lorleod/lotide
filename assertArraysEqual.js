bb5293b3f41d6cdc1c6e9932b89ba605195301de
lorleod <taylor.mcleod@gmail.com>
2022-05-04 17:51:33 +0000

    created arrays equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,37 @@
+// Function: takes in two arrays and returns true or false, based on a perfect match
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Take in two arrays and console.log and asserts passed if equal or failed if not equal
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  // input arrayOne and arrayTwo into eqArrays.
+  // If true, then console log passed. if false, console log failed
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`✅✅✅ Arrays EQUAL: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Arrays NOT equal: ${arrayOne} !== ${arrayTwo}`);
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
